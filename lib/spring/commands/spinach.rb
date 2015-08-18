67c684558a7558515c5a040c61be0544ac594033
Yi Wen <ywen.github@gmail.com>
2015-08-18 11:37:40 -0500

    Allow env is being different than test

--- lib/spring/commands/spinach.rb
+++ lib/spring/commands/spinach.rb
@@ -2,15 +2,16 @@ module Spring
   module Commands
     class Spinach
       def env(*)
-        "test"
+        ENV["SPINACH_ENV"] || "test"
       end
 
       def exec_name
         "spinach"
       end
+
     end
 
     Spring.register_command "spinach", Spinach.new
-    Spring::Commands::Rake.environment_matchers[/^spinach($|:)/] = "test"
+    Spring::Commands::Rake.environment_matchers[/^spinach($|:)/] = Spring::Commands::Spinach.new.env
   end
 end
